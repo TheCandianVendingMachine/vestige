08bd4c2bafdedc7b711dd6bd91a53adf05f54805
Maya Woodward <mayawoodward0@gmail.com>
2023-09-15 23:43:52 -0400

    Add basic rand functions (and abs)

--- include/lib.h
+++ include/lib.h
@@ -1,9 +1,10 @@
 #ifndef VESTIGE_LIB_H
 #define VESTIGE_LIB_H
 
-#include "lib/hashmap.h"
-#include "lib/vector.h"
-#include "lib/string.h"
-#include "lib/result.h"
+#include <lib/hashmap.h>
+#include <lib/math.h>
+#include <lib/result.h>
+#include <lib/string.h>
+#include <lib/vector.h>
 
 #endif
