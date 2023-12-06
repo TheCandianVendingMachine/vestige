0b2719d00231f03bf82de7da23be03081416ffe5
Bailey Danyluk <baileydanyluk@gmail.com>
2023-12-05 17:38:00 -0700

    add vestige logging

--- src/lib/filesystem.c
+++ src/lib/filesystem.c
@@ -2,14 +2,16 @@
 #include <stdio.h>
 #include <time.h>
 #include <utime.h>
+#include <errno.h>
 
 #include "lib/filesystem.h"
+#include "logger.h"
 
 time_t get_modify_time(const char* file) {
     struct stat file_data;
 
     if (stat((const char*)file, &file_data) < 0) {
-        perror((const char*)file);
+        log_warning("%s: %s", file, strerror(errno));
         return 0;
     }
     return file_data.st_mtime;
@@ -18,7 +20,7 @@ time_t get_modify_time(const char* file) {
 FileMetaData file_meta_data(const char* file) {
     return (FileMetaData) {
         .file_path = string_from_cstr((char*)file),
-        .last_modified = time_from_seconds(get_modify_time(file))
+        .last_modified = time_from_milliseconds(1000 * get_modify_time(file))
     };
 }
 
@@ -27,9 +29,16 @@ void destroy_file_meta_data(FileMetaData* file) {
 }
 
 void update_file_modify_time(FileMetaData* file) {
-    file->last_modified = time_from_seconds(get_modify_time((const char*)file->file_path.buffer));
+    const char* file_str = cstr_from_string(file->file_path);
+    file->last_modified = time_from_milliseconds(1000 * get_modify_time(file_str));
+    free((char*)file_str);
 }
 
 bool has_file_been_modified(FileMetaData file) {
-    return time_as_seconds(file.last_modified) == get_modify_time((const char*)file.file_path.buffer);
+    const char* file_str = cstr_from_string(file.file_path);
+    time_t modify_time = get_modify_time(file_str);
+    free((char*)file_str);
+    Time t = time_from_milliseconds(modify_time);
+    log_debug("%d %d %d %d", modify_time, time_as_seconds(file.last_modified), t._seconds, t._nanoseconds);
+    return time_as_seconds(file.last_modified) != modify_time;
 }
