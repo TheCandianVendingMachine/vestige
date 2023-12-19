afddfd494eaaecaab8c43a7c3943f06faebccef9
Bailey Danyluk <baileydanyluk@gmail.com>
2023-12-18 19:00:11 -0700

    tabs->spaces

--- src/lib/vector.c
+++ src/lib/vector.c
@@ -1,29 +1,29 @@
 #include <lib/vector.h>
 
 Vector new_vector(uint32_t itemsize, size_t size) {
-	uint8_t* buffer = malloc(size * itemsize);
-	if (buffer == NULL) {
-		perror("new_vector: ");
-		exit(1);
-	}
-	Vector v = {
-		.length = 0,
-		._size = size,
-		._itemsize = itemsize,
-		.buffer = buffer,
-	};
-	return v;
+    uint8_t* buffer = malloc(size * itemsize);
+    if (buffer == NULL) {
+        perror("new_vector: ");
+        exit(1);
+    }
+    Vector v = {
+        .length = 0,
+        ._size = size,
+        ._itemsize = itemsize,
+        .buffer = buffer,
+    };
+    return v;
 }
 
 void del_vector(Vector v) {
-	free(v.buffer);
+    free(v.buffer);
 }
 
 void vector_grow(Vector* v) {
-	v->_size *= 2;
-	v->buffer = realloc(v->buffer, 2 * v->_size * v->_itemsize);
-	if (v->buffer == NULL) {
-		perror("vector_grow: ");
-		exit(1);
-	}
+    v->_size *= 2;
+    v->buffer = realloc(v->buffer, 2 * v->_size * v->_itemsize);
+    if (v->buffer == NULL) {
+        perror("vector_grow: ");
+        exit(1);
+    }
 }
